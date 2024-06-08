429473f090162688d852c96f5c79cebde859a1da
Simon Qi <smilexx2@users.noreply.github.com>
2024-06-08 06:12:57 +0000

    update jest config to remove warnings

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        babel: true,
+        tsConfig: 'tsconfig.json',
+      },
+    ],
   },
 };
